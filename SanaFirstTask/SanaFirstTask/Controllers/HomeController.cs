1e337067edb80d672a0d58d8684132a25d30604b
Eugune Biletskyi <biletskijzena@protonmail.com>
2023-03-26 20:47:07 +0300

    Completed first task

---
+++ SanaFirstTask/SanaFirstTask/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SanaFirstTask.Models;
+using System.Diagnostics;
+
+namespace SanaFirstTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Entername()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Entername(string name)
+        {
+            ViewData["name"] = name;    
+            return View("Output");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
